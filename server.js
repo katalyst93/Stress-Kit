556aa28bcf8546a126162f98ecefd045a7d346be
Shums Kassam <skassam21@gmail.com>
2018-06-24 11:15:45 -0400

    images are in

--- server.js
+++ server.js
@@ -16,40 +16,48 @@ const client = new Wit({
 var faultsList = {
   'all_or_nothing_thinking': {
     'name': 'All or nothing thinking',
-    'description': 'Perfectionism is the enemy. Reach for 80% and you will find you get further towards your goals.'
+    'description': 'Perfectionism is the enemy. Reach for 80% and you will find you get further towards your goals.',
+    'image': 'dist/images/allornothing.png'
   },
   'generalization': {
     'name': 'Generalization',
-    'description': 'One bad experience does not mean that all experiences will be the same.'
+    'description': 'One bad experience does not mean that all experiences will be the same.',
+    'image': 'dist/images/overgeneralization.png'
   },
   'mind_reading': {
     'name': 'Mind Reading',
-    'description': 'Negative prejudices, poor self image and assumptions.....if you are guessing what others are thinking/feeling, you need to ask questions.'
+    'description': 'Negative prejudices, poor self image and assumptions.....if you are guessing what others are thinking/feeling, you need to ask questions.',
+    'image': 'dist/images/mindreading.png'
   },
   'fortune_telling': {
     'name': 'Fortune Telling',
-    'description': 'Look for patterns. Do you have enough info to make this statement.'
+    'description': 'Look for patterns. Do you have enough info to make this statement.',
+    'image': 'dist/images/fortunetelling.png'
   },
   'catastrophizing': {
     'name': 'Catastrophizing',
-    'description': 'Life is made up of infinite outcomes. The world has not ended. What resources do you have to build a stronger argument.'
+    'description': 'Life is made up of infinite outcomes. The world has not ended. What resources do you have to build a stronger argument.',
+    'image': 'dist/images/catastrophising.png'
   },
   'emotional_reasoning': {
     'name': 'Emotional Reasoning',
-    'description': 'Emotions are not facts. What facts could disrupt your reasoning here ?'
+    'description': 'Emotions are not facts. What facts could disrupt your reasoning here?',
+    'image': 'dist/images/EmotionalReasoning.png'
   },
   'labelling': {
-    'name': 'Labeling',
+    'name': 'Labelling',
     'description': 'Putting things into a cage makes them scary things. If they break out, avoid labeling.',
     'image': 'dist/images/labeling.png'
   },
   'personalization': {
     'name': 'Personalization',
-    'description': 'Burst your bubble and share responsibility. Look for outside factors beyond those involved.'
+    'description': 'Burst your bubble and share responsibility. Look for outside factors beyond those involved.',
+    'image': 'dist/images/personalization_blame.png'
   },
   'shoulds': {
-    'name': 'Shoulds',
-    'description': 'Implies consequences that you can\'t have total control over.'
+    'name': 'Should, Ought to',
+    'description': 'Implies consequences that you can\'t have total control over.',
+    'image': 'dist/images/should_must.png'
   }
 }
 
@@ -85,8 +93,17 @@ app.get('/check-phrase', function (req, res) {
 
   client.message(phrase, {})
     .then((data) => {
-      for (key in data.entities) {
-        faults.push(faultsList[key])
+      if (Object.keys(data.entities).length > 1) {
+        for (key in data.entities) {
+          console.log(key);
+          if (key !== 'generalization') {
+            faults.push(faultsList[key])
+          }
+        }
+      } else {
+        for (key in data.entities) {
+          faults.push(faultsList[key])
+        }
       }
 
       respond(res, faults);
